0e805ce13efacee86b429574c717f1cdc37d6d1c
Jimmy Chandra <jimmycdinata@gmail.com>
2014-07-05 08:53:35 +0700

    ADD clone header and body

--- app/views/index.phtml.php
+++ app/views/index.phtml.php
@@ -6,18 +6,36 @@
 				
 			
 		
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="keywords" content="cord blood banking, stemcord indonesia, bank darah tali pusat" />
+		<meta name="descriptions" content="cord blood banking, stemcord indonesia, bank darah tali pusat" />
+		<meta name="robots" content="all" />
 		<?php echo $this->tag->stylesheetLink('css/font.css'); ?>
-		<?php echo $this->tag->stylesheetLink('css/styles.css'); ?>
+		
 		
 		<?php echo $this->tag->stylesheetLink('css/style/reset.css'); ?>
 		<?php echo $this->tag->stylesheetLink('css/style/superfish.css'); ?>
 		<?php echo $this->tag->stylesheetLink('css/style/jquery.fancybox.css'); ?>
 		<?php echo $this->tag->stylesheetLink('css/style/jquery.qtip.css'); ?>
 		<?php echo $this->tag->stylesheetLink('css/style/jquery-ui-1.9.2.custom.css'); ?>
-		<?php echo $this->tag->stylesheetLink('css/responsive.css'); ?>
 		
+		<?php echo $this->tag->stylesheetLink('css/styles.css?v=1.02'); ?>
+		<?php echo $this->tag->stylesheetLink('css/responsive.css?v=1.02'); ?>
 
+		<?php echo $this->tag->javascriptInclude('js/jquery-1.8.3.min.js?ver=3.6'); ?>
+		<?php echo $this->tag->javascriptInclude('js/jquery.ba-bbq.min.js'); ?>
+		<?php echo $this->tag->javascriptInclude('js/jquery-ui-1.9.2.custom.min.js'); ?>
+		
+		<?php echo $this->tag->javascriptInclude('js/jquery.easing.1.3.js'); ?>
+		<?php echo $this->tag->javascriptInclude('js/jquery.carouFredSel-5.6.4-packed.js'); ?>
+		<?php echo $this->tag->javascriptInclude('js/jquery.sliderControl.js'); ?>
+		<?php echo $this->tag->javascriptInclude('js/jquery.linkify.js'); ?>
+		<?php echo $this->tag->javascriptInclude('js/jquery.hint.js'); ?>
+		<?php echo $this->tag->javascriptInclude('js/jquery.isotope.min.js'); ?>
+		<?php echo $this->tag->javascriptInclude('js/jquery.isotope.masonry.js'); ?>
+		<?php echo $this->tag->javascriptInclude('js/jquery.fancybox-1.3.4.pack.js'); ?>
+		<?php echo $this->tag->javascriptInclude('js/jquery.blockUI.js'); ?>
+		<?php echo $this->tag->javascriptInclude('js/jquery.selectbox-0.2.1.min.js'); ?>
+		<?php echo $this->tag->javascriptInclude('js/main.js'); ?>
 
        
 	</head>
