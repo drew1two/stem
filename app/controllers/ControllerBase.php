ed66038df6da8afbb7a5ae150103746d319490a8
Jimmy Chandra <jimmycdinata@gmail.com>
2014-07-05 17:30:08 +0700

    FIX dimension for width and height image

--- app/controllers/ControllerBase.php
+++ app/controllers/ControllerBase.php
@@ -15,6 +15,7 @@ class ControllerBase extends Controller
 	protected function initialize()
 	{
 		$this->view->setVar('phoneSimple', '021-520-6555/6');
+		$this->view->setVar('faxSimple', '021-520-6551');
 		$this->view->setVar('phoneCall', '+62215206555');
 		$this->view->setVar('companyName', 'Stemcord Indonesia');
 	}
