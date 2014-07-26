287727398ba3952258fe7ee0b5f74044ffa7e29c
Jimmy Chandra <jimmycdinata@gmail.com>
2014-07-26 12:28:01 +0700

    ADD friend 2,3, and 4 for friend-refer-friend

--- app/controllers/LayananPelangganController.php
+++ app/controllers/LayananPelangganController.php
@@ -57,12 +57,41 @@ class LayananPelangganController extends ControllerBase
                     'motherContact'		=> strtoupper($this->request->getPost('referal-mothers-contact', 'striptags')),
                     'motherEmail'		=> strtolower($this->request->getPost('referal-mothers-email', 'striptags')),
                     'nameFriend1'		=> strtoupper($this->request->getPost('referal-friend1-name', 'striptags')),
-                    'emailFriend1'		=> strtoupper($this->request->getPost('referal-friend1-email', 'striptags')),
+                    'emailFriend1'		=> strtolower($this->request->getPost('referal-friend1-email', 'striptags')),
                     'contactFriend1'	=> strtoupper($this->request->getPost('referal-friend1-contact', 'striptags')),
                     'hospitalFriend1'	=> strtoupper($this->request->getPost('referal-friend1-hospital', 'striptags')),
                     'contactPreference'	=> strtoupper($contactPreference),
 					);
+
+				if ($this->request->getPost('referal-friend2-name')) {
+                	$content = array_merge($content, array (
+                		'nameFriend2'		=> strtoupper($this->request->getPost('referal-friend2-name', 'striptags')),
+	                    'emailFriend2'		=> strtolower($this->request->getPost('referal-friend2-email', 'striptags')),
+	                    'contactFriend2'	=> strtoupper($this->request->getPost('referal-friend2-contact', 'striptags')),
+	                    'hospitalFriend2'	=> strtoupper($this->request->getPost('referal-friend2-hospital', 'striptags')),
+                	));
+                }
+
+                if ($this->request->getPost('referal-friend3-name')) {
+                	$content = array_merge($content, array (
+                		'nameFriend3'		=> strtoupper($this->request->getPost('referal-friend3-name', 'striptags')),
+	                    'emailFriend3'		=> strtolower($this->request->getPost('referal-friend3-email', 'striptags')),
+	                    'contactFriend3'	=> strtoupper($this->request->getPost('referal-friend3-contact', 'striptags')),
+	                    'hospitalFriend3'	=> strtoupper($this->request->getPost('referal-friend3-hospital', 'striptags')),
+                	));
+                }
+
+                if ($this->request->getPost('referal-friend4-name')) {
+                	$content = array_merge($content, array (
+                		'nameFriend4'		=> strtoupper($this->request->getPost('referal-friend4-name', 'striptags')),
+	                    'emailFriend4'		=> strtolower($this->request->getPost('referal-friend4-email', 'striptags')),
+	                    'contactFriend4'	=> strtoupper($this->request->getPost('referal-friend4-contact', 'striptags')),
+	                    'hospitalFriend4'	=> strtoupper($this->request->getPost('referal-friend4-hospital', 'striptags')),
+                	));
+                }
+                
 				$template = $this->getTemplate('referfriend', $content);
+
 
                 $mg = new Mailgun($this->config->mail->mailgunApiKey);
                 $domain = $this->config->mail->mailgunDomain;
